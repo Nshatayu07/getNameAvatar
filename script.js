7e1df4706531ce257ebbec86e4cca58a1ac17c32
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-09-12 02:26:41 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function getName(){
+    var gname = document.querySelector('input[name = "name"]').value;
+    // alert(gname.value);
+    console.log(getName.value);
+    const img = document.querySelector("#imageChange");
+    img.src = `https://joesch.moe/api/v1/${gname}`;
+    
+    return gname;
+}
+
+/*let en;
+en.addEventListener('keyup', (e)=>{
+    if(e.keyCode === 13){
+        console.log(e.gname);
+        getName();
+    }
+})*/    
+
+var btn = document.querySelector("button");
+btn.addEventListener("click", getName);
+
